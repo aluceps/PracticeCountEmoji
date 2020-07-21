67c2cb49b9373f724179593ab4e113032e0e4be0
mery-takano <atsushi.takano@mery.co.jp>
2020-07-22 03:08:09 +0900

    add viewmodel and livedata

--- app/src/main/java/me/aluceps/practicetextview/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicetextview/MainActivity.kt
@@ -1,8 +1,13 @@
 package me.aluceps.practicetextview
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import me.aluceps.practicetextview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +16,30 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.root
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        setupObserver()
     }
+
+    private fun setupObserver() {
+        viewModel.inputData.observe(this, Observer { text ->
+            Log.d("###", "text=$text")
+            viewModel.inputDataCount.postValue("%d".format(text.length))
+        })
+
+        viewModel.inputDataCount.observe(this, Observer {
+            Log.d("###", "count=$it")
+        })
+    }
+}
+
+class MainViewModel : ViewModel() {
+    val inputData = MutableLiveData<String>()
+    val inputDataCount = MutableLiveData<String>()
 }
