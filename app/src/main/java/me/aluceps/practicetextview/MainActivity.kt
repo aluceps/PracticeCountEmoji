d5add041ae04dccd6ea1d21db562ee529eb38d15
mery-takano <atsushi.takano@mery.co.jp>
2020-07-22 02:01:31 +0900

    enable databinding

--- app/src/main/java/me/aluceps/practicetextview/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicetextview/MainActivity.kt
@@ -1,12 +1,18 @@
 package me.aluceps.practicetextview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import me.aluceps.practicetextview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding
     }
 }
